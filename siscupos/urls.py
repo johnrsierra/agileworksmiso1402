8a51d848245367c12bb46794d5139afbf8466a29
diego <die-agud@uniandes.edu.co>
2014-09-06 16:22:56 -0500

    adicion de paginas estudiantes y materias

--- siscupos/urls.py
+++ siscupos/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from siscupos import views
 urlpatterns = patterns('',
     url(r'^$', views.index , name='index'),
-
+    url(r'^materias/$',views.materias, name='materias'),
+    url(r'^estudiantes/$',views.estudiantes, name='estudiantes'),
 )
 
--- siscupos/views.py
+++ siscupos/views.py
@@ -10,4 +10,14 @@ def index(request):
     grupos = 1
     ubicaciones = 1
     context = {'nuevos_contactos':contactos,'nuevos_grupos':grupos,'nuevas_ubicaciones':ubicaciones}
-    return render(request,'index.html', {})
+    return render(request,'index.html', context)
+
+def materias(request):
+    lista_materias = Asignatura.objects.all()
+    context = {'lista_materias':lista_materias}
+    return render(request,'materias/lista_materias.html',context)
+
+def estudiantes(request):
+    lista_estudiantes = Estudiante.objects.all()
+    context = {'lista_estudiantes':lista_estudiantes}
+    return render(request,'estudiantes/lista_estudiantes.html',context)
