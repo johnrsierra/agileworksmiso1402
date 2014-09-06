3d599f2693ad082779b19577070913a646915b96
diego <die-agud@uniandes.edu.co>
2014-09-06 12:42:57 -0500

    venv added

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,9 +47,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'siscupos.urls'
+ROOT_URLCONF = 'mysite.urls'
 
-WSGI_APPLICATION = 'siscupos.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
