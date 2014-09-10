93a421494aef9ffb3351f28e82c85b1c74e69109
diego <die-agud@uniandes.edu.co>
2014-09-09 21:37:20 -0500

    added admin

--- siscupos/urls.py
+++ siscupos/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib import admin
+admin.autodiscover()
 from siscupos import views
 urlpatterns = patterns('',
     url(r'^$', views.index , name='index'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^programas/$',views.programas, name='programas'),
     url(r'^programas/demanda/$',views.demandaCupos, name='demanda'),
     url(r'^materias/$',views.materias, name='materias'),
