4498a5723ad6056d615ffee915547bb458977d69
diego <die-agud@uniandes.edu.co>
2014-09-22 23:51:14 -0500

    support to DDrop

--- mysite/static/js/siscupos.js
+++ mysite/static/js/siscupos.js
@@ -1,1 +1,12 @@
 'use strict';
+$( "#stack .materia" ).draggable({revert:true, clone:true,stack:'#stack .materia' });
+ $( "div[id^='periodo']" ).droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    accept: ":not(.ui-sortable-helper)",
+    drop: function( event, ui ) {
+        console.log(ui.draggable[0]);
+        var tmp = ui.draggable[0]
+        $(tmp).clone().removeAttr('style').appendTo(this);
+    }
+ });
